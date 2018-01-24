cb7b34fa375d5434a3ccb83cac8cd6916ad63bc3
jackzheng2496 <jackzheng2496@gmail.com>
2018-01-24 16:12:20 -0500

    cannot reliably compare floating points

--- tests/distance_test.c
+++ tests/distance_test.c
@@ -12,9 +12,6 @@ Test(distance_test, simple) {
 
 	result = distance(a, b);
 
-	int x = (int)(EXPECTED_DISTANCE * 1000000);
-	int y = (int)(result * 1000000);
-
-	cr_assert_eq(y, x,
+	cr_assert_eq(result, EXPECTED_DISTANCE,
 	"Distance not what was expected. Expected: [%.6f] Actual: [%.6f]\n", EXPECTED_DISTANCE, result);
 }
